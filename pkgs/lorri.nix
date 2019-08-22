d245d031e34ee4134ca79ed38095f07db38bb5ef
Tobias Happ <tobias.happ@gmx.de>
2019-08-22 20:07:44 +0200

    Set fixed commit for lorri

--- pkgs/lorri.nix
+++ pkgs/lorri.nix
@@ -1,5 +1,6 @@
 { }:
 
 import (fetchTarball {
-  url = "https://github.com/target/lorri/archive/rolling-release.tar.gz";
+  url = "https://github.com/target/lorri/archive/38eae3d487526ece9d1b8c9bb0d27fb45cf60816.tar.gz";
+  sha256 = "11k9lxg9cv6dlxj4haydvw4dhcfyszwvx7jx9p24jadqsy9jmbj4";
 }) { }
