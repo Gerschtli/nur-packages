4a18e2f246d06eecb65c4c3b50bcf14234c73682
Tobias Happ <tobias.happ@gmx.de>
2019-12-14 21:21:06 +0100

    Add build expression for dwm-status

--- pkgs/dwm-status/default.nix
+++ pkgs/dwm-status/default.nix
@@ -1,14 +1,33 @@
-{ pkgs, fetchFromGitHub, useGlobalAlsaUtils }:
+{ stdenv, lib, rustPlatform, fetchFromGitHub
+, coreutils, dnsutils, iproute, wirelesstools, alsaUtils
+, makeWrapper, pkgconfig, dbus, gdk_pixbuf, libnotify, xorg
+, useGlobalAlsaUtils }:
 
 let
   config = builtins.fromJSON (builtins.readFile ./config.json);
 
-  dwm-status = fetchFromGitHub {
+  binPath = stdenv.lib.makeBinPath (
+    [ coreutils dnsutils iproute wirelesstools ]
+    ++ lib.optional (!useGlobalAlsaUtils) alsaUtils
+  );
+in
+
+rustPlatform.buildRustPackage rec {
+  inherit (config) cargoSha256;
+
+  name = "dwm-status";
+
+  src = fetchFromGitHub {
     inherit (config) rev sha256;
 
     owner = "Gerschtli";
     repo = "dwm-status";
   };
-in
 
-import dwm-status { inherit pkgs useGlobalAlsaUtils; }
+  nativeBuildInputs = [ makeWrapper pkgconfig ];
+  buildInputs = [ dbus gdk_pixbuf libnotify xorg.libX11 ];
+
+  postInstall = ''
+    wrapProgram $out/bin/${name} --prefix "PATH" : "${binPath}"
+  '';
+}
