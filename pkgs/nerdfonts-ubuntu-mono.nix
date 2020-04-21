18f355e293a95695c3c16304ab0fc7244a3fae6a
Tobias Happ <tobias.happ@gmx.de>
2020-04-21 23:33:49 +0200

    Use fonts arg for nerdfonts if present

--- pkgs/nerdfonts-ubuntu-mono.nix
+++ pkgs/nerdfonts-ubuntu-mono.nix
@@ -1,5 +1,8 @@
 { pkgs }:
 
-pkgs.nerdfonts.override {
-  withFont = "UbuntuMono";
-}
+pkgs.nerdfonts.override (
+  # in unstable channels use fonts arg
+  if pkgs.nerdfonts.override.__functionArgs ? "fonts"
+  then { fonts = [ "UbuntuMono" ]; }
+  else { withFont = "UbuntuMono"; }
+)
