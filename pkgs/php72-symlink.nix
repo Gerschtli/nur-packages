cefdd38421cb29aaa3f06cafd03c4d672265af2f
Tobias Happ <tobias.happ@gmx.de>
2020-04-23 11:36:24 +0200

    Fix build for unstable nixpkgs

--- pkgs/php72-symlink.nix
+++ pkgs/php72-symlink.nix
@@ -1,7 +1,7 @@
-{ php72 }:
+{ lib, php72 }:
 
 php72.overrideAttrs (old: {
-  postInstall = old.postInstall + ''
-    ln -snf $out/bin/php $out/bin/php72
-  '';
+  postInstall =
+    lib.optionalString (old ? postInstall) old.postInstall
+    + "ln -snf $out/bin/php $out/bin/php72";
 })
