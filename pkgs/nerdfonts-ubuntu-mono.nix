001c5f4f1fdf1d3d1895093b4134774ca3c151b1
Tobias Happ <tobias.happ@gmx.de>
2020-04-23 10:44:48 +0200

    Simplify nix expression

--- pkgs/nerdfonts-ubuntu-mono.nix
+++ pkgs/nerdfonts-ubuntu-mono.nix
@@ -1,8 +1,8 @@
-{ pkgs }:
+{ nerdfonts }:
 
-pkgs.nerdfonts.override (
+nerdfonts.override (
   # in unstable channels use fonts arg
-  if pkgs.nerdfonts.override.__functionArgs ? "fonts"
+  if nerdfonts.override.__functionArgs ? "fonts"
   then { fonts = [ "UbuntuMono" ]; }
   else { withFont = "UbuntuMono"; }
 )
