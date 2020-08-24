ccc2fa0966d8b97fcd7be4ef6b61cc79673deea9
Tobias Happ <tobias.happ@gmx.de>
2020-08-24 13:27:15 +0200

    Use another nixpkgs rev for php72

--- pkgs/php72.nix
+++ pkgs/php72.nix
@@ -1,20 +1,22 @@
 { callPackage, fetchFromGitHub, fetchurl, pkgs }:
 
 let
-  # one commit before php 7.2 drop
-  # https://github.com/NixOS/nixpkgs/commit/e6001db0f72b5be755b759298d111538c5b90365
+  # working commit before php72 drop
+  # https://github.com/NixOS/nixpkgs/commit/e9bd19d4d352f45e871cfc3e91f854cb76ff48e1
   pinnedPkgs = import (fetchFromGitHub {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "e6001db0f72b5be755b759298d111538c5b90365";
-    sha256 = "0fw35pz6hc6galn8d4bfwshl6r41p0h9m4vy85nqx2n24y6r5qk7";
+    rev = "e9bd19d4d352f45e871cfc3e91f854cb76ff48e1";
+    sha256 = "07pwvd45ra4d4xlhb7m5z04faqm2s51w4bk6yl750gy96w6cmnqk";
   }) {
     config = { };
     overlays = [ ];
   };
 
   php72 = pinnedPkgs.php72.overrideAttrs (old: {
-    postInstall = "ln -snf $out/bin/php $out/bin/php72";
+    postInstall = old.postInstall + ''
+      ln -snf $out/bin/php $out/bin/php72
+    '';
   });
 
   packages = callPackage
@@ -25,4 +27,4 @@ let
     };
 in
 
-php72 // { inherit (packages) extensions packages; }
+php72 // { inherit packages; }
