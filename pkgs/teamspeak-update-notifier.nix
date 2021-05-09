5ad9cafd8d94d7c5b54fb6f81613974672fc1661
Tobias Happ <tobias.happ@gmx.de>
2021-05-09 15:11:33 +0200

    Update teamspeak-update-notifier

--- pkgs/teamspeak-update-notifier.nix
+++ pkgs/teamspeak-update-notifier.nix
@@ -2,13 +2,13 @@
 
 python39.pkgs.buildPythonPackage rec {
   pname = "teamspeak-update-notifier";
-  version = "1.6.0";
+  version = "1.7.0";
 
   src = fetchFromGitHub {
     owner = "Gerschtli";
     repo = "teamspeak-update-notifier";
     rev = version;
-    sha256 = "sha256-l0XfdYd8hN95WLVKRSIy8Ltw6gDurD1jNQvZvw4zdZ0=";
+    sha256 = "sha256-CyO6CXWIvb74cFQ3rQ21zWAWdUxN97SqqfcBX3uEoOY=";
   };
 
   propagatedBuildInputs = with python39Packages; [
